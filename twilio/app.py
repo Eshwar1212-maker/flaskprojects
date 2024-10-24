961a6993b17e1a1e2342901c5667db222bbd7585
Eshwar Tangirala <eshwartangirala11@gmail.com>
2024-10-24 19:02:50 -0400

    Added endpoint to create a room

--- twilio/app.py
+++ twilio/app.py
@@ -9,20 +9,29 @@ from flask import Flask, render_template, request
 
 load_dotenv()
 
-account_sid = os.environ()
-
 #Create a twilio client
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-api_key = os.environ["TWILIO_API_SID"]
+api_key = os.environ["TWILIO_API_KEY_SID"]
 api_secret = os.environ["TWILIO_API_KEY_SECRET"]
 twilio_client = twilio.rest.Client(api_key, api_secret, account_sid)
 
+app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello Flask"
+def serve_homepage():
+    return "Home page"
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+#Request to 
+def find_or_create_room(room_name):
+    try:
+        #try to fetch an in-progress room with this name
+        twilio_client.video.rooms(room_name).fetch()
+    except:
+        #the room did not exist, so create it
+        twilio_client.video.rooms.create(unique_name=room_name, type="go")
+
+        
 
